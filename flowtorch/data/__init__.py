4fce08226d7dae92cc51a403a4d173b09876d142
Sebastian Spinner <sebastian.spinner@dlr.de>
2023-06-27 14:42:53 +0200

    Added new class to init.py

--- flowtorch/data/__init__.py
+++ flowtorch/data/__init__.py
@@ -4,6 +4,7 @@ from .csv_dataloader import CSVDataloader
 from .vtk_dataloader import VTKDataloader
 from .psp_dataloader import PSPDataloader
 from .tau_dataloader import TAUDataloader, TAUConfig
+from .tau_surfacedataloader import TAUSurfaceDataloader
 from .selection_tools import mask_box, mask_sphere
 from .outlier_tools import iqr_outlier_replacement
 try:
